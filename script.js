8872b954c8f7c4ee0a0b472c02407d919b165e3d
iurilugao01 <128332733+iurilugao01@users.noreply.github.com>
2024-01-03 12:09:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ document.querySelector("#d12").addEventListener("click", function () {
   const resultado = roll(12);
   result.value = resultado;
 });
+document.querySelector("#d8").addEventListener("click", function () {
+  const resultado = roll(8);
+  result.value = resultado;
+});
+document.querySelector("#d6").addEventListener("click", function () {
+  const resultado = roll(6);
+  result.value = resultado;
+});
+document.querySelector("#d4").addEventListener("click", function () {
+  const resultado = roll(4);
+  result.value = resultado;
+});
+document.querySelector("#d100").addEventListener("click", function () {
+  const resultado = roll(100);
+  result.value = resultado;
+});
