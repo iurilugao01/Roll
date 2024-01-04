d450feb3d64b7aff2a88244c4033f9e129e7e973
iurilugao01 <128332733+iurilugao01@users.noreply.github.com>
2024-01-04 17:37:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-const result = document.querySelector("#result");
 function roll(X) {
   const rollN = Math.floor(Math.random() * X) + 1;
-  return rollN;
+  const result = document.querySelector("#result");
+  result.value = rollN;
 }
 
-document.querySelector("#d20").addEventListener("click", function () {
-  const resultado = roll(20);
-  result.value = resultado;
-});
-document.querySelector("#d12").addEventListener("click", function () {
-  const resultado = roll(12);
-  result.value = resultado;
-});
-document.querySelector("#d8").addEventListener("click", function () {
-  const resultado = roll(8);
-  result.value = resultado;
-});
-document.querySelector("#d6").addEventListener("click", function () {
-  const resultado = roll(6);
-  result.value = resultado;
-});
-document.querySelector("#d4").addEventListener("click", function () {
-  const resultado = roll(4);
-  result.value = resultado;
-});
-document.querySelector("#d100").addEventListener("click", function () {
-  const resultado = roll(100);
-  result.value = resultado;
+document.querySelectorAll(".dd").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const typeDd = btn.getAttribute("data-dd");
+    roll(parseFloat(typeDd));
+  });
 });
