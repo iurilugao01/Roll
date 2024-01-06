07d5be5c9f93d9bf05ab1c303fd600449be5ce7a
iurilugao01 <128332733+iurilugao01@users.noreply.github.com>
2024-01-06 18:04:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-function roll(X) {
-  const rollN = Math.floor(Math.random() * X) + 1;
-  const result = document.querySelector("#result");
-  result.value = rollN;
-}
+let typeRoll = "default";
+let rollNumber = 1;
 
 document.querySelectorAll(".dd").forEach(function (btn) {
   btn.addEventListener("click", function () {
-    const typeDd = btn.getAttribute("data-dd");
-    roll(parseFloat(typeDd));
+    const result = document.querySelector("#result");
+
+    if (typeRoll === "default") {
+      const typeDd = btn.getAttribute("data-dd");
+      result.value = roll(parseFloat(typeDd));
+    }
+
+    if (typeRoll === "bestValue") {
+      const typeDd = btn.getAttribute("data-dd");
+      result.value = rollBestValue(typeDd, rollNumber);
+    }
   });
 });
+
+document.querySelector("#switch").addEventListener("click", function () {
+  const radio = document.getElementsByName("dado");
+  const radioArray = Array.from(radio);
+  const rollValue = parseFloat(document.querySelector("#rollNumber").value);
+
+  radioArray.forEach(function (radio) {
+    if (radio.checked) {
+      typeRoll = radio.value;
+    }
+  });
+
+  if (rollValue) {
+    rollNumber = rollValue;
+  }
+});
+
+const roll = (X) => Math.floor(Math.random() * X) + 1;
+
+const rollBestValue = (typeRoll, quantity) => {
+  if (quantity <= 0) {
+    alert("A quantidade de dados deve ser maior que zero.");
+    return;
+  }
+
+  const results = Array.from({ length: quantity }, () => roll(typeRoll));
+  let bestValue = Math.max(...results);
+
+  console.log(results);
+  return bestValue;
+};
